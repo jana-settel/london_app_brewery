2ab544dbbf72c45e54c5d820e5aa6badbc37bb8e
Jana Settel <zettelyana@gmail.com>
2021-12-29 19:42:27 +0100

    Implement logic and UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,101 @@
+// ignore: unused_import
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      const MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFADCCE0),
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            'Ask me',
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+          backgroundColor: const Color(0xFFADCCE0),
+        ),
+        body: const BallPagex(),
+      ),
+    ),
+  );
+}
+
+class BallPagex extends StatefulWidget {
+  const BallPagex({Key? key}) : super(key: key);
+
+  @override
+  _BallPagexState createState() => _BallPagexState();
+}
+
+class _BallPagexState extends State<BallPagex> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                  // ignore: avoid_print
+                  print(
+                      "Hey Janosita, this is the value of ballNumber: $ballNumber");
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+
+// class BallPage extends StatefulWidget {
+//   const BallPage({Key? key}) : super(key: key);
+
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+// }
+// class _BallPageState extends State<BallPage> {
+
+
+  
+//   @override
+//   noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
+// }
+
+//   Widget build(BuildContext context) {
+      
+//     return Center(
+//       child: Row(
+//         children: <Widget>[
+//           Expanded(
+//             child: TextButton(
+//               onPressed: () {
+//                 setState(() {
+//                   ballNumber = Random().nextInt(5) + 1;
+//                 });
+//               },
+//               child: Image.asset('images/ball$ballNumber.png'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void setState(Null Function() param0) {}
+//  }
+
